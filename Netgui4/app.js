5fd02e6c102661025b96c885d82b5c79be6245d0
EnriqueEstebaranz <enriqueestebaranzredondo@hotmail.com>
2024-05-20 17:17:28 +0200

    pausa y reanudar

--- Netgui4/app.js
+++ Netgui4/app.js
@@ -1,5 +1,7 @@
 // Para que se seleccionen colores distintos y que no se pueda enviar dos paquetes del mismo color a la vez.
 let coloresActivos = new Set();
+let escalaY = 0;
+
 
 function colorAleatorio() {
     let colorHex;
@@ -19,21 +21,65 @@ function liberarColor(color) {
     coloresActivos.delete(color);
 }
 
+AFRAME.registerComponent("reproducir", {
+    init: function() {
+        this.el.addEventListener("click", () => {
+            // Asegurarse de que el componente envio-paquetes existe
+            const envioEntity = document.querySelector('[envio-paquetes]');
 
+            document.querySelectorAll("[animation]").forEach((element) => {
+                element.emit('animation-resume');
+            });
+
+            envioEntity.components['envio-paquetes'].reanudar();
+            console.log("holaaa" + document.querySelector('[historial]'));
+            console.log("quetalll"+document.querySelector('[historial]').components);
+
+            // Asegurarse de que el componente simulacro existe
+            const historialEntidad = document.querySelector('[historial]');
+            historialEntidad.components['historial'].reanuda();
+
+
+        });
+    }
+});
+
+AFRAME.registerComponent("pausa", {
+    init: function() {
+        this.el.addEventListener("click", () => {
+            // Asegurarse de que el componente envio-paquetes existe
+
+            console.log("eeeeewse" + document.querySelector('[envio-paquetes]'));
+            console.log("queteeeeeealll"+document.querySelector('[envio-paquetes]').components);
+            const envioEntity = document.querySelector('[envio-paquetes]');
+            envioEntity.components['envio-paquetes'].pausar();
+
+            // Emitir evento de pausa a todos los elementos con animaciones
+            document.querySelectorAll("[animation]").forEach((element) => {
+                element.emit('animation-pause');
+            });
+
+            console.log("holaaa" + document.querySelector('[historial]'));
+            console.log("quetalll"+document.querySelector('[historial]').components);
+            const historialEntidad = document.querySelector('[historial]');
+            historialEntidad.components['historial'].pausado();
+        });
+    }
+});
 
 // Componente difuminado
 AFRAME.registerComponent('difuminado',{
     schema: {
         tiempo: {type: "number", default:4000},
-        opacidad: {type: "number", default:0.1}
-        },
-        init: function () {
-            this.el.setAttribute('animation__fadeout', {
-                property: 'material.opacity',
-                to: this.data.opacidad,
-                dur: this.data.tiempo,
-                easing: 'linear'
-            });
+        opacidad: {type: "number", default:0.4}
+    },
+    init: function () {
+        this.el.setAttribute('animation__fadeout', {
+            property: 'material.opacity',
+            to: this.data.opacidad,
+            dur: this.data.tiempo,
+            easing: 'linear'
+        });
     }
 });
 
@@ -46,29 +92,46 @@ AFRAME.registerComponent("trazador", {
     init: function () {
 
 
-        const escenario = document.querySelector("[escenario]");
+        const historial = document.querySelector("[historial]");
         const material = this.el.getAttribute('material')
         const color = material.color
         console.log(this.data.intervalo)
-        this.traza(escenario, color)
+        this.traza(historial, color)
 
     },
-    traza:function(escenario, color){
+    traza:function(historial, color){
         console.log("dsdsdsds")
         console.log(this.interval)
         this.interval = setInterval(()=>{
             const posicion = this.el.getAttribute('position')
-            const posicionEscenario = escenario.getAttribute('position')
+            const posicionHistorial = historial.getAttribute('position')
             const entidad = document.createElement('a-entity');
             if (this.data.forma == 'caja'){
                 entidad.setAttribute('geometry', 'primitive: box; width: 0.4; height: 0.1; depth: 0.4');
+                entidad.setAttribute('material', 'color', color);
+                entidad.setAttribute('position', `${posicion.x} ${posicionHistorial.y} ${posicion.z}`);
+                entidad.setAttribute('difuminado', '');
             } else if (this.data.forma == 'esfera'){
                 entidad.setAttribute('geometry', 'primitive: sphere; radius: 0.05');
+                entidad.setAttribute('material', 'color', color);
+                entidad.setAttribute('position', `${posicion.x} ${posicionHistorial.y} ${posicion.z}`);
+                entidad.setAttribute('difuminado', '');
+            } else if (this.data.forma == 'pc'){
+                entidad.setAttribute('geometry', {primitive: "box", width:0.2, height: 1, depth:0.2 })
+                entidad.setAttribute('position', `${posicion.x} ${posicionHistorial.y} ${posicion.z}`);
+                entidad.setAttribute("material", {src: "#pc", side: "double"});
+            } else if (this.data.forma == 'router'){
+                entidad.setAttribute('geometry', {primitive: "cylinder", radius:0.2, height: 1})
+                entidad.setAttribute('material', { color: "#808080" })
+                entidad.setAttribute('position', `${posicion.x} ${posicionHistorial.y} ${posicion.z}`);
+                
+            } else if (this.data.forma == 'hub'){
+                entidad.setAttribute('geometry', {primitive: "box", width:0.2, height: 1, depth:0.2 })
+                entidad.setAttribute("material", {src: "#hub", side: "double"});
+                entidad.setAttribute('position', `${posicion.x} ${posicionHistorial.y} ${posicion.z}`);
+                
             }
-            entidad.setAttribute('material', 'color', color);
-            entidad.setAttribute('position', `${posicion.x} ${posicionEscenario.y} ${posicion.z}`);
-            entidad.setAttribute('difuminado', '');
-            document.querySelector('a-scene').appendChild(entidad);
+            document.querySelector("a-scene").appendChild(entidad);
         },this.data.intervalo)
     },
     remove: function() {
@@ -80,63 +143,154 @@ AFRAME.registerComponent("trazador", {
 
 // Componente encargado de la creación y el envio de paquetes
 AFRAME.registerComponent("envio-paquetes", {
-    
-    // init se llama automaticamente en cuanto el componente "____" se inicializa
-    init: function(){  // Configuración inicial para el funcionamiento del componente
-        const escenario = document.querySelector("[escenario]");
-        this.envioPaquetes(escenario)
+    almacenTimeouts: [], // Almacenamiento de timeouts
+    pausa: false,
+
+    init: function() {
+        this.escenario = document.querySelector("[escenario]");  // Asegurarse de que el escenario está definido
+        this.envioPaquetes();
     },
 
-    envioPaquetes: function(escenario){
+    envioPaquetes: function() {
         fetch('packages.json')
-            .then(response => response.json())
-            .then(json => {
-            const paquetes = json.packages
-                paquetes.forEach(paquete => {
-                    setTimeout(() => {
-                        if (paquete.route.length < 2){
-                            liberarColor(color);
-                            console.error("Ruta no valida")
-                            return
-                        }
-                        // Introduzco el paquete en el escenario
-                        const pqt = document.createElement('a-entity')
-                        const color = colorAleatorio(); 
-                        pqt.setAttribute('geometry', {primitive: "box", width:0.3, height:0.3, depth:0.3});
-                        pqt.setAttribute('material', {color: color });
-
-                        escenario.appendChild(pqt)
-                        const salida = document.getElementById(paquete.route[0])
-                        pqt.object3D.position.copy(salida.object3D.position);
-
-                        pqt.setAttribute('trazador',{forma: 'esfera', intervalo: 30});
-                        const movimiento = (i) => {
-                            if (i >= paquete.route.length){
-                                // Meter sonido de fin de envio o algo
-                                escenario.removeChild(pqt);
-                                return;
-                            }
-                            const siguiente = document.getElementById(paquete.route[i]);
-                            if (siguiente){
-                                pqt.setAttribute('animation', {
-                                    property: 'position',
-                                    to: `${siguiente.object3D.position.x} ${siguiente.object3D.position.y} ${siguiente.object3D.position.z}`,
-                                    dur:2000,
-                                    easing: 'linear'
-                                });
-                            }
-                            pqt.addEventListener('animationcomplete', () => movimiento(i+1), {once: true}); 
-                        };
-                        movimiento(1);
-                    }, paquete.time * 1000);
-                })
+        .then(response => response.json())
+        .then(json => {
+            json.packages.forEach(paquete => {
+                const delay = paquete.time * 1000;
+                const timeout = setTimeout(() => {
+                    if (!this.pausa) {
+                        this.animacion(paquete);
+                    }
+                    // Elimina el timeout del almacen después de ejecutarse
+                    this.almacenTimeouts = this.almacenTimeouts.filter(t => t.id !== timeout);
+                }, delay);
+                this.almacenTimeouts.push({ id: timeout, package: paquete, delay: delay, startTime: Date.now(), remainingTime: delay });
             })
-            .catch(error => console.error('Error al cargar packages.json:', error));
+        })
+        .catch(error => console.error('Error loading packages.json:', error));
+    },
+
+    animacion: function(paquete) {
+        if (paquete.route.length < 2) {
+            console.error("Ruta no valida");
+            return;
+        }
+        const pqt = document.createElement('a-entity');
+        const color = colorAleatorio(); 
+        pqt.setAttribute('geometry', {primitive: "box", width:0.3, height:0.3, depth:0.3});
+        pqt.setAttribute('material', {color: color });
+
+        this.escenario.appendChild(pqt);
+        const salida = document.getElementById(paquete.route[0]);
+        pqt.object3D.position.copy(salida.object3D.position);
+
+        pqt.setAttribute('trazador', {forma: 'esfera', intervalo: 30});
+        const movimiento = (i) => {
+            if (i >= paquete.route.length) {
+                this.escenario.removeChild(pqt);
+                liberarColor(color);
+                return;
+            }
+            const siguiente = document.getElementById(paquete.route[i]);
+            if (siguiente) {
+                pqt.setAttribute('animation', {
+                    property: 'position',
+                    to: `${siguiente.object3D.position.x} ${siguiente.object3D.position.y} ${siguiente.object3D.position.z}`,
+                    dur: 2000,
+                    easing: 'linear',
+                    resumeEvents: 'animation-resume',
+                    pauseEvents: 'animation-pause'
+                });
+                pqt.addEventListener('animationcomplete', () => movimiento(i+1), {once: true}); 
+            }
+        };
+        movimiento(1);
+    },
+
+    pausar: function() {
+        this.pausa = true;
+        this.almacenTimeouts = this.almacenTimeouts.map(t => {
+            clearTimeout(t.id);  // Cancela el timeout actual
+            const currentTime = Date.now();
+            console.log("aqui")
+            console.log(t.remainingTime)
+            t.remainingTime = t.remainingTime - (currentTime - t.startTime);  // Calcula el tiempo restante
+            console.log("despues")
+            console.log(t.remainingTime)
+            return t
+        });
+        console.log(this.almacenTimeouts)
+    },
+
+    reanudar: function() {
+        this.pausa = false;
+        this.almacenTimeouts.forEach(t => {
+
+            const timeout = setTimeout(() => {
+                this.animacion(t.package);
+                // Elimina el timeout del almacen después de ejecutarse
+                this.almacenTimeouts = this.almacenTimeouts.filter(t => t.id !== timeout);
+            }, t.remainingTime);
+            t.startTime = Date.now();
+            t.id = timeout; // Actualizar el ID del timeout
+            
+        });
+        console.log(this.almacenTimeouts)
+    }
+});
+
+AFRAME.registerComponent("historial", {
+    schema: {
+        movimientoVertical: {type: "number", default: 0.05},
+        tiempo: {type: "number", default: 50}
+
+    },
+
+    pausa: false,
+    interval: null,
+
+    init: function () {
+        
+        this.movimientohistorial()
+        console.log(this.data.movimientoVertical)
+
+    },
+
+
+    
+    pausado: function() {
+        this.pausa = true;
+        if (this.interval) {
+            clearInterval(this.interval);
+            this.interval = null;
+        }
+    },
+
+    reanuda: function() {
+        this.pausa = false;
+        this.movimientohistorial();
+        
+    },
+
+    movimientohistorial: function() {
+        
+        this.interval = setInterval(() => {
+            escalaY -= this.data.movimientoVertical
+            //console.log("estamos por aqui")
+            //console.log(escalaY)
+            if (!this.pausa){
+                this.el.setAttribute('position', `0 ${escalaY} 0`);
+            }
+            
+        }, this.data.tiempo); 
     }
 })
 
 // Componente encargado de la creación del mapa en el que se realiza el envio
 AFRAME.registerComponent("simulacro",{
+    pausa: false,
+    interval: null,
+
     init:function () {
         this.el.addEventListener("click",()=>{
             // creo la entidad donde se localizará todo el escenario
@@ -157,17 +311,16 @@ AFRAME.registerComponent("simulacro",{
             this.construirMapa(escenario);
 
             // Función que mete los paquetes y lo envia entre nodos
-            this.crearpaquete(escenario)
+            this.crearpaquete(escenario);
 
-
-            const posicionEscenario = escenario.getAttribute('position')
-            let initialY = 0; // Comienza en la posición Y = 0
-            setInterval(() => {
-                initialY += 0.02; // Incrementa en 1 metro cada segundo
-                escenario.setAttribute('position', `0 ${initialY} 0`);
-            }, 20); // Actualiza cada 1000 milisegundos (1 segundo)
+            this.crearhistorial(escenario);
+            
         })
     },
+
+
+
+
     
     // Añado los controles (pausa y reprodución) que se quedaran fijos en la camara.
     agregarControles: function(){
@@ -210,7 +363,9 @@ AFRAME.registerComponent("simulacro",{
                     entidad.object3D.position.set((node.position[0]), 0, (node.position[1]))
                     entidad.setAttribute("material", {src: "#pc", side: "double"});
                     entidad.setAttribute("id", node.name)
+                    entidad.setAttribute('trazador', {forma: 'pc', intervalo: 1000});
                     escenario.appendChild(entidad);
+                    
                 } else if (node.type.includes(hub)){
                 //} else if (node.type == "NKHub"){
                     const entidad = document.createElement('a-entity');
@@ -219,6 +374,7 @@ AFRAME.registerComponent("simulacro",{
                     entidad.object3D.position.set((node.position[0]), 0, (node.position[1]))
                     entidad.setAttribute("material", {src: "#hub", side: "double"});
                     entidad.setAttribute("id", node.name)
+                    entidad.setAttribute('trazador', {forma: 'hub', intervalo: 1000});
                     escenario.appendChild(entidad);
                 } else if (node.type.includes(router)){
                 //} else if (node.type == "NKRouter"){
@@ -228,6 +384,7 @@ AFRAME.registerComponent("simulacro",{
                     entidad.object3D.position.set((node.position[0]), 0, (node.position[1]))
                     entidad.setAttribute('material', { color: "#808080" });
                     entidad.setAttribute("id", node.name)
+                    entidad.setAttribute('trazador', {forma: 'router', intervalo: 1000});
                     escenario.appendChild(entidad); 
                 }
                 escenario.setAttribute('escalarMapa', 'velocidad: 1');
@@ -256,9 +413,13 @@ AFRAME.registerComponent("simulacro",{
         entidad.setAttribute('envio-paquetes','');
         entidad.setAttribute("id", "envio")
         escenario.appendChild(entidad);
-        console.log("holitaaaa")
+    },
+    crearhistorial: function (escenario){
+        const historial = document.createElement("a-entity")
+        historial.setAttribute('historial',{movimientoVertical: 0.05, tiempo: 50})
+        historial.setAttribute("id", "historieta")
+        escenario.appendChild(historial);
     }
-
 });
 
 
